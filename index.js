b2f292a3cad4ea76379fb10c6cac9c6a3edf9cda
Myat Phone Paing <myatphonepaingpussi223@gmail.com>
2023-04-04 16:23:15 -0700

    commit two

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const port = 4000;
-const users = require("./router/user");
+const users = require("./router/userRouter");
+const menulist = require("./router/menuRouter");
 
-// app.use(express.static("public"));
+const customers = [
+  { name: "mo mo", email: "momo@gmail.com", age: 20 },
+  { name: "pai", email: "pai@gmail.com", age: 21 },
+  { name: "paing", email: "paing@gmail.com", age: 22 },
+];
+app.use(express.static("public"));
 // app.set("views", "./views");
 // app.set("view engine", "pug");
+
+console.log(process.env);
 
 // app.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname, "script.js"));
@@ -21,11 +29,12 @@ const users = require("./router/user");
 //   res.send("world");
 // });
 
-// app.get("/user", (req, res) => {
-//   res.send("user");
+// app.get("/", (req, res) => {
+//   res.send();
 // });
 
 app.use("/user", users);
+app.use("/menu", menulist);
 
 // app.get("/momo", (req, res) => {
 //   res.sendFile(`${__dirname}/mo mo.jpeg`);
--- public/script.js
+++ public/script.js
@@ -1,9 +1,8 @@
-const url = "http://localhost:4000/";
+const url = "http://localhost:4000/menu/menulist";
 const fetchData = async () => {
   const response = await fetch(url);
-  const data = await response.json;
-  console.log("fetching data is...", data);
+  const data = await response.json();
+  console.log(data);
 };
-
 fetchData();
 console.log("I am ");
